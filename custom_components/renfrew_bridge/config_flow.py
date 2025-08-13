4eb09ec2282260a73fad2ece4973ef2cb8c6a225
Chris <64163980+thenextbutton@users.noreply.github.com>
2025-08-14 00:27:23 +0100

    Update config_flow.py
    removed the set refresh on integration setup, adding it back in.

--- custom_components/renfrew_bridge/config_flow.py
+++ custom_components/renfrew_bridge/config_flow.py
@@ -1,9 +1,7 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 
-from .const import DOMAIN
-from .options_flow import RenfrewBridgeOptionsFlowHandler
+from .const import DOMAIN, CONF_REFRESH_MINUTES, DEFAULT_REFRESH_MINUTES
 
 class RenfrewBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Renfrew Bridge config flow."""
@@ -13,12 +11,20 @@ class RenfrewBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial setup step."""
         if user_input is not None:
-            return self.async_create_entry(title="Renfrew Bridge", data={})
-        
-        return self.async_show_form(step_id="user")
+            return self.async_create_entry(
+                title="Renfrew Bridge", 
+                data={CONF_REFRESH_MINUTES: user_input[CONF_REFRESH_MINUTES]}
+            )
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this handler."""
-        return RenfrewBridgeOptionsFlowHandler(config_entry)
+        data_schema = vol.Schema({
+            vol.Required(
+                CONF_REFRESH_MINUTES,
+                default=DEFAULT_REFRESH_MINUTES
+            ): vol.All(vol.Coerce(int), vol.Range(min=0, max=60))
+        })
+        
+        return self.async_show_form(
+            step_id="user", 
+            data_schema=data_schema, 
+            errors={}
+        )
